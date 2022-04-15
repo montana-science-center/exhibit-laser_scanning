09f707d58c328a355fab9039cbae34a7299f4e15
Kevin <kevinw@virtualmatter.org>
2022-04-15 03:43:06 -0400

    Fix time quantization issue and start work on fast ADC read.

--- galvos/wave_generator/wave_generator.ino
+++ galvos/wave_generator/wave_generator.ino
@@ -1,4 +1,3 @@
-#include <FastLED.h> // 3.5.0
 #include <digitalWriteFast.h>
 
 // DAC AD7528
@@ -7,27 +6,45 @@
 // digital pin 8 is DAC select (x/y) (port B), low is A
 // digital pin 9 is write enable (port B), low is active
 
-#define dac_select_pin 8
-#define write_enable_pin 9
-#define DACA 0
-#define DACB 1
+const byte dac_select_pin = 8;
+const byte write_enable_pin = 9;
+const byte adc_x_pin = A0;
+const byte adc_y_pin = A1;
+const byte DACA = 0;
+const byte DACB = 1;
 
-// needs fine frequency control.
-// try scaling 12 bit form adc to 0-100hz
-byte x_freq_hz = 10;
-byte y_freq_hz = 11;
+byte x_freq_hz = 127;
+byte y_freq_hz = 52;
+
+// sine wavetable
+const int wavetable_samples = 256;
+byte wavetable[wavetable_samples];
 
 
 void setup() {
+
+    // init wavetable
+    float half_res = wavetable_samples / 2.0;
+    for (int i = 0; i < wavetable_samples; i++) {
+        wavetable[i] = (byte)(sin( (i / half_res) * M_PI) * 127) + 128;
+    }
 
     // Port D to all output
     //      MSB..LSB
     // pins 9      0
     DDRD = B11111111;
 
+    // ADC setup
+    // http://www.gammon.com.au/adc
+    ADCSRA =  bit (ADEN);   // turn ADC on
+    ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits
+    ADCSRA |= bit (ADPS2);                               //  16
+    ADMUX  = bit (REFS0) | (adc_x_pin & 0x07);  // AVcc and select input port
+
+
     // port B pins 8 and 9 to output
-    pinModeFast(dac_select_pin, OUTPUT);
-    pinModeFast(write_enable_pin, OUTPUT);
+    pinMode(dac_select_pin, OUTPUT);
+    pinMode(write_enable_pin, OUTPUT);
 
     // init pin values
     PORTD = 0;
@@ -39,20 +56,12 @@ void setup() {
 void loop() {
     byte x_pos = wave8(x_freq_hz);
     byte y_pos = wave8(y_freq_hz);
-
-    // X
     write_dac(DACA, x_pos);
-    // Y
     write_dac(DACB, y_pos);
 }
 
 
 void write_dac(bool ab_select, byte value) {
-    // select, DAC,
-    // pull write low,
-    // dump data to port D,
-    // pull write high.
-
     digitalWriteFast(dac_select_pin, ab_select);
     digitalWriteFast(write_enable_pin, LOW);
     PORTD = value;
@@ -61,15 +70,7 @@ void write_dac(bool ab_select, byte value) {
 
 
 uint8_t wave8(uint8_t hz) {
-    // GET_MILLIS is uint32_t
-
-    // Hz is 'beats per second', or 'cycles per 1000ms'.
-    // To avoid using the (slower) division operator, we
-    // want to convert 'cycles per 1000ms' to 'cycles per 65536ms',
-    // and then use a simple, fast bit-shift to divide by 65536.
-    //
-    // The ratio 65536:1000 is 4294967:256
-
-    uint8_t beat = ((GET_MILLIS()) * hz * 4294967) >> 24;
-    return sin8(beat);
+    // micros is a uint32_t
+    uint8_t beat = ((micros()) * hz * 4294) >> 24;
+    return wavetable[beat];
 }
