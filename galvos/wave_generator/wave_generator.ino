e75b43e08a8feff00ec7a51dc331cc457c729dbb
Kevin <kevinw@virtualmatter.org>
2022-04-17 23:54:40 -0400

    Switched galvos to encoder.  Still phase issue to work out.

--- galvos/wave_generator/wave_generator.ino
+++ galvos/wave_generator/wave_generator.ino
@@ -1,5 +1,13 @@
 #include <digitalWriteFast.h>
 
+// Encoders used because ADC read and denoise was too slow.
+// Interrups are on the only 8-bit port used for the laser DAC
+// so we can't use them for the encoders without port-spanning :(
+#define ENCODER_DO_NOT_USE_INTERRUPTS
+#include <Encoder.h>
+Encoder x_encoder(10, 11);
+Encoder y_encoder(12, 13);
+
 // DAC AD7528
 // digital pins 0-7 write DAC value (port D)
 // chip select is always low (selected)
@@ -8,13 +16,13 @@
 
 const byte dac_select_pin = 8;
 const byte write_enable_pin = 9;
-const byte adc_x_pin = A0;
-const byte adc_y_pin = A1;
 const byte DACA = 0;
 const byte DACB = 1;
 
-byte x_freq_hz = 127;
-byte y_freq_hz = 52;
+byte x_freq_hz = 255; 
+byte pre_x_freq_hz = 255;
+byte y_freq_hz = 255;
+byte pre_y_freq_hz = 255;
 
 // sine wavetable
 const int wavetable_samples = 256;
@@ -34,14 +42,6 @@ void setup() {
     // pins 9      0
     DDRD = B11111111;
 
-    // ADC setup
-    // http://www.gammon.com.au/adc
-    ADCSRA =  bit (ADEN);   // turn ADC on
-    ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits
-    ADCSRA |= bit (ADPS2);                               //  16
-    ADMUX  = bit (REFS0) | (adc_x_pin & 0x07);  // AVcc and select input port
-
-
     // port B pins 8 and 9 to output
     pinMode(dac_select_pin, OUTPUT);
     pinMode(write_enable_pin, OUTPUT);
@@ -54,10 +54,24 @@ void setup() {
 }
 
 void loop() {
-    byte x_pos = wave8(x_freq_hz);
-    byte y_pos = wave8(y_freq_hz);
+    byte x_pos = wave8(x_freq_hz, pre_x_freq_hz);
+    byte y_pos = wave8(y_freq_hz, pre_y_freq_hz);
     write_dac(DACA, x_pos);
     write_dac(DACB, y_pos);
+    x_freq_hz = read_encoder(x_encoder);
+    //y_freq_hz = read_encoder(y_encoder);
+}
+
+byte read_encoder(Encoder &the_encoder) {
+    int state = the_encoder.read() >> 2;
+    if (state > 255) {
+        state = 255;
+        the_encoder.write(255 << 2);
+    }else if (state < 0) {
+        state = 0;
+        the_encoder.write(0);
+    }
+    return (byte)state;
 }
 
 
@@ -69,8 +83,15 @@ void write_dac(bool ab_select, byte value) {
 }
 
 
-uint8_t wave8(uint8_t hz) {
+uint8_t wave8(uint8_t hz, uint8_t &pre_hz) {
     // micros is a uint32_t
-    uint8_t beat = ((micros()) * hz * 4294) >> 24;
+    uint8_t beat = ((micros()) * pre_hz * 4294) >> 24;
+    
+    if (hz != pre_hz) {
+        if (beat == 0){
+            pre_hz = hz;
+        }
+    }
+    
     return wavetable[beat];
 }
